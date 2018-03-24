fc89dde4b4287122b0fc3d8010db3fbed391ae1d
Michele Sciabarra <sciabarracom@sciabarra.com>
2018-03-24 09:47:32 +0000

    creating 2 images, one base image goproxy and another for golang actions

---
+++ test/README.md
@@ -0,0 +1,8 @@
+# Test
+
+How to run tests:
+
+- install `cram`: `sudo pip install cram`
+- start the tester: `cd test ; ./start.sh`
+- in another terminal `cd test ; cram test.t`
+
--- test/start.sh
+++ test/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $(dirname $0)
+cd "$(dirname $0)"
 bin/build.sh src/hello_greeting.go 
 bin/build.sh src/hello_message.go 
 bin/build.sh src/empty.go
@@ -8,7 +8,10 @@ rm -Rvf action
 if test -n "$1"
 then go run ../main/proxy.go -debug
 else
-  go build -o ../docker/proxy ../main/proxy.go
-  docker build -t action-golang-v1.9 ../docker/
+  #go build -o ../docker/proxy ../main/proxy.go
+  #docker build -t action-golang-v1.9 ../docker/
+  cd ..
+  ./gradlew distDocker
+  cd test
   docker run -ti -p 8080:8080  action-golang-v1.9 "$@" 
 fi
