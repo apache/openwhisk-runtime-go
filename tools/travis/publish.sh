a429735d2eeaac84db4ba2c4acd55ff0ccebb63d
Carlos Santana <csantanapr@apache.org>
2018-05-09 01:22:35 -0400

    publish action loop also

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -21,14 +21,8 @@ set -eux
 # Build script for Travis-CI.
 
 IMAGE_PREFIX=$1
-RUNTIME_VERSION=$2
+RUNTIME=$2
 IMAGE_TAG=$3
-
-if [ ${RUNTIME_VERSION} == "1.10" ]; then
-  RUNTIME="golang1.10"
-elif [ ${RUNTIME_VERSION} == "1.11" ]; then
-  RUNTIME="golang1.11"
-fi
 
 if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
