83ebb19eb0a841720e7533e3481e9497f96feae9
Michele Sciabarra <sciabarracom@sciabarra.com>
2018-10-09 22:34:38 +0200

    fixed build.sh

--- openwhisk/_test/build.sh
+++ openwhisk/_test/build.sh
@@ -19,48 +19,38 @@
 cd "$(dirname $0)"
 
 function build {
-   if test -e $1
-   then return
-   fi
+   test -e exec && rm exec
    cp $1.src $1.go
    GOPATH=$PWD go build -a -o exec $1.go
    rm $1.go
 }
 
 function build_main {
-   if test -e $1
-   then return
-   fi
+   test -e exec && rm exec
    cp ../../common/gobuild.py.launcher.go $1.go
    cat $1.src >>$1.go
    go build -a -o exec $1.go
    rm $1.go
 }
 
-function zipit {
-    if test -e $1
-    then return
-    fi
-    mkdir $$
-    cp $2 $$/$3
-    zip -q -j $1 $$/$3
-    rm -rf $$
-}
-
-build exec
-rm exec.zip 2>/dev/null
-zip -q -r exec.zip exec etc dir
 
 build hi
-zipit hi.zip hi exec
+zip hi.zip exec
+cp exec hi
 
 build_main hello_message
-zipit hello_message.zip hello_message exec
+zip hello_message.zip exec
+cp exec hello_message
 
 build_main hello_greeting
-zipit hello_greeting.zip hello_greeting exec
+zip hello_greeting.zip exec
+cp exec hello_greeting
 
-rm hello.zip 2>/dev/null
+test -e hello.zip && rm hello.zip
 cd src
 zip -q -r ../hello.zip main.go hello
 cd ..
+
+build exec
+test -e exec.zip && rm exec.zip
+zip -q -r exec.zip exec etc dir
