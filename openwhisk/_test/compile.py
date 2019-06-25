ebd6f439650f1bf92f7bf488f4eaedb88b9d1ea8
Matt Rutkowski <mrutkows@us.ibm.com>
2019-06-25 11:56:26 -0500

    ASF full header added to remaining files (#95)

--- openwhisk/_test/compile.py
+++ openwhisk/_test/compile.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
-# Licensed to the Apache Software Foundation (ASF) under one or more contributor
-# license agreements; and to You under the Apache License, Version 2.0.
-import os, sys
-os.rename(sys.argv[2], sys.argv[3]+"/action")
-with open(sys.argv[3]+"/exec", "w") as f:
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import sys
+
+os.rename(sys.argv[2], sys.argv[3] + "/action")
+
+with open(sys.argv[3] + "/exec", "w") as f:
     f.write("""#!/bin/bash
 cd "$(dirname $0)"
 export PYTHONPATH=$PWD/action
--- openwhisk/_test/pysample/lib/exec.py
+++ openwhisk/_test/pysample/lib/exec.py
@@ -1,20 +1,37 @@
-# Licensed to the Apache Software Foundation (ASF) under one or more contributor
-# license agreements; and to You under the Apache License, Version 2.0.
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from __future__ import print_function
-import os, json
+
+import os
+import json
+
 from action.main import main
 inp = os.fdopen(0, "rb")
 out = os.fdopen(3, "wb")
 while True:
     while True:
-       line = inp.readline()
-       args = json.loads(line)
-       payload = {}
-       if "value" in args:
-           payload = args["value"]
-       res = main(payload)
-       out.write(json.dumps(res, ensure_ascii=False).encode('utf-8'))
-       out.write("\n")
-       out.flush()
-
+        line = inp.readline()
+        args = json.loads(line)
+        payload = {}
+        if "value" in args:
+            payload = args["value"]
+        res = main(payload)
+        out.write(json.dumps(res, ensure_ascii=False).encode('utf-8'))
+        out.write("\n")
+        out.flush()
