f9fcf527238f6015959b6c2dfc7b668ce6945343
Michele Sciabarra <msciabarra@apache.org>
2020-01-24 16:27:44 +0100

    err in stderr

--- openwhisk/initHandler.go
+++ openwhisk/initHandler.go
@@ -120,7 +120,7 @@ func (ap *ActionProxy) initHandler(w http.ResponseWriter, r *http.Request) {
 		if os.Getenv("OW_LOG_INIT_ERROR") == "" {
 			sendError(w, http.StatusBadGateway, err.Error())
 		} else {
-			ap.outFile.Write([]byte(err.Error() + "\n"))
+			ap.errFile.Write([]byte(err.Error() + "\n"))
 			ap.outFile.Write([]byte(OutputGuard))
 			ap.errFile.Write([]byte(OutputGuard))
 			sendError(w, http.StatusBadGateway, "The action failed to generate or locate a binary. See logs for details.")
